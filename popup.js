f6bf4130a97d8eeb7d1951588365ef158426031d
Iamam305 <84980608+Iamam305@users.noreply.github.com>
2021-11-03 20:37:21 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,18 @@
+
+const app = Vue.createApp({
+data(){
+    return{
+        name:"ajitesh",
+        number:"",
+        link:"http://web.whatsapp.com/send?phone="
+    };
+},
+});
+
+app.mount("body");
+    // "background": {
+
+    //     "service_worker": "popup.js"
+        
+    // }
+ 
